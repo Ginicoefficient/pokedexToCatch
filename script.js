e0acdb2343f7596c0221cc9ca19efb0a828952f1
Ginicoefficient <104734969+Ginicoefficient@users.noreply.github.com>
2023-02-27 18:21:06 -0600

    added styling of pokemonCards

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const dsTopContainer = document.getElementById("top-ds-container");
 async function load() {
   const localStorageValue = localStorage.getItem("storedPokeObjects");
   if (!localStorageValue) {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=15");
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon/?limit=151"
+    );
     const data = await response.json();
     pokeObjects = data.results;
   } else {
@@ -48,44 +50,57 @@ document.addEventListener("click", (e) => {
 
 async function displayPokeObject(id) {
   let pokemonCard = "";
-  if (!pokeObjects[id - 1].detailedInfo) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await response.json();
-    pokemonCard = data;
-    pokeObjects[id - 1].detailedInfo = pokemonCard;
+  //if there's no specific info about pokemon in local storage, fetch & store
+  if (!pokeObjects[id - 1].abilities) {
+    const [response1, response2] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
+    ]);
+    const specificInfo = await response1.json();
+    const speciesInfo = await response2.json();
+
+    pokemonCard = {
+      ...specificInfo,
+      ...speciesInfo["flavor_text_entries"][0],
+    };
+    pokeObjects[id - 1] = pokemonCard;
     updateLocalStorage();
   } else {
-    pokemonCard = pokeObjects[id - 1].detailedInfo;
+    pokemonCard = pokeObjects[id - 1];
   }
-  //make a property on each pokeObject that holds html?? Maybe
-  //multiple types on a pokemon requires mapping
+  console.log(pokemonCard);
 
+  //use info from fetch or local storage to render info about the selecte mon
   const pokemonCardHtml = `
-  <div>${pokemonCard.name}
-  ${pokemonCard.height}
-  ${pokemonCard.weight}
-  ${pokemonCard.types[0].type.name}
-  <img src="${pokemonCard.sprites.versions["generation-vi"]["omegaruby-alphasapphire"]["front_default"]}"/>
-  </div>`;
+  <div class="pkm-card">
+    <div class="pkm-card-hdr pixel-corners">
+        <h3>${pokemonCard.name}</h3>
+        <img class="pokemon-img" src="${pokemonCard.sprites.versions["generation-iii"]["emerald"]["front_default"]}"/></div>
+    <hr class="divider">
+    <div class="pkm-card-body pixel-corners">
+        <div class="pkm-card-flavortext">${pokemonCard.flavor_text}</div>
+    </div>
+    <hr class="divider">
+    <div class="pkm-card-footer">
+       Height: ${pokemonCard.height}in / 
+       Weight: ${pokemonCard.weight}lb / 
+       Type: ${pokemonCard.types[0].type.name}
+        <div><i class="fa-solid fa-plus btn add-btn" id=${pokemonCard.id}></i></div></div>
+    </div>`;
 
   document.getElementById("bottom-ds-container").innerHTML = pokemonCardHtml;
 }
 
-//gets id, checks if info is in local storage object, then render or fetch/render
-//in bottom of dex
-//add button will be here rendered with pokemon ID
-
-const addPokemonToCatch = (id) => {
+const addPokemonToCatchList = (id) => {
+  //grab id of parent container which is pokemon
+  //grab information pokemonobject and put in array of CatchPokemon
+  //give propoerty isCaught, set equal to false
+  //save CatchPokemon to local storage
   console.log(id);
 };
 
 //when added, pokemon object gets added to t0-catch array
 //put into local storage
 
-//pokemon class could probably make to render specific elements of pokemon information
-//There is a button for clicking to add them to your "to catch"
-
 //to catch list is an array holding info about pokemon, gets mapped to html
 //button to move them to your caught list (your pc!!)
-
-//pokedex style, grass/route style, computer/pc style for the respective sections
